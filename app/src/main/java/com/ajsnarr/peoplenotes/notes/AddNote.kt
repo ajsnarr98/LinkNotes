5582beb60fb90585ea3af22bad0921a6b8c3e9ef
AJ Snarr <ajsnarr98@gmail.com>
2019-05-09 14:24:30 -0400

    added files in place

--- app/src/main/java/com/ajsnarr/peoplenotes/notes/AddNote.kt
+++ app/src/main/java/com/ajsnarr/peoplenotes/notes/AddNote.kt
@@ -1,12 +1,13 @@
-package com.ajsnarr.peoplenotes
+package com.ajsnarr.peoplenotes.notes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ajsnarr.peoplenotes.R
 
-class MainActivity : AppCompatActivity() {
+class AddNote : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_addnote)
     }
 }
---
+++ app/src/main/java/com/ajsnarr/peoplenotes/placeholder.kt
@@ -0,0 +1,4 @@
+package com.ajsnarr.peoplenotes
+
+class placeholder {
+}

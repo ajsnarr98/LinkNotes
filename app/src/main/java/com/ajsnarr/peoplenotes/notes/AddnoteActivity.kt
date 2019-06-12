3165f7e552f3e7ddd7bb1aa78684af7a10a2edd4
AJ Snarr <ajsnarr98@gmail.com>
2019-06-12 13:40:19 -0400

    Refactor/seperate activities (#7)
    * separated addnote and search into two activities

--- app/src/main/java/com/ajsnarr/peoplenotes/notes/AddnoteActivity.kt
+++ app/src/main/java/com/ajsnarr/peoplenotes/notes/AddnoteActivity.kt
@@ -1,14 +1,12 @@
 package com.ajsnarr.peoplenotes.notes
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -22,51 +20,62 @@ val entries = mutableListOf<Entry>(
     Entry("12345"),
     Entry("123456"))
 
-class AddnoteFragment : Fragment() {
+class AddnoteActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var recyclerAdapter: EntryAdapter
 
     lateinit var numEntriesText: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_addnote, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addnote)
 
         // add to autocomplete text view
-        val noteType = view.findViewById<AutoCompleteTextView>(R.id.autocompletetext_addnote_notetype)
-        val adapter = ArrayAdapter(activity!!,
+        val noteType = findViewById<AutoCompleteTextView>(R.id.autocompletetext_addnote_notetype)
+        val adapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_1, NOTE_TYPES
         )
         noteType.setAdapter(adapter)
 
         // set up tags popup window
-        val tagsBtn = view.findViewById<Button>(R.id.tags_popup_btn)
-        val containingView = view.findViewById<View>(R.id.layout_addnote_popupcontainer)
-        EditNoteTagsPopup(activity!!.layoutInflater, containingView, getScreenSize(activity!!), tagsBtn)
+        val tagsBtn = findViewById<Button>(R.id.tags_popup_btn)
+        val containingView = findViewById<View>(R.id.layout_addnote_popupcontainer)
+        EditNoteTagsPopup(layoutInflater, containingView, getScreenSize(this), tagsBtn)
 
         // number of entries text
-        numEntriesText = view.findViewById(R.id.text_addnote_numentries)
+        numEntriesText = findViewById(R.id.text_addnote_numentries)
         updateNumEntriesText()
 
 
         // set up recycler view
-        val recyclerManager = LinearLayoutManager(activity)
+        val recyclerManager = LinearLayoutManager(this)
         recyclerAdapter = EntryAdapter(entries)
 
-        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_addnote_entries).apply {
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_addnote_entries).apply {
             layoutManager = recyclerManager
             setAdapter(recyclerAdapter)
         }
 
         // set up add entry button
-        val addEntryBtn = view.findViewById<Button>(R.id.button_editnote_addentry)
+        val addEntryBtn = findViewById<Button>(R.id.button_editnote_addentry)
         addEntryBtn.setOnClickListener {
             entries.add(Entry.newEmpty())
             recyclerAdapter.notifyDataSetChanged()
             updateNumEntriesText()
         }
+    }
 
-        return view
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_settings -> return true // TODO
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun updateNumEntriesText() {

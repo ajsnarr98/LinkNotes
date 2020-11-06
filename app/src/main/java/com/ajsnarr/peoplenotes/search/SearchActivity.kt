c46cd913fb7490ed4c45f44ab42ffd10de68bb95
AJ Snarr <aj.snarr@willowtreeapps.com>
2020-11-06 11:06:59 -0500

    added nav drawer

--- app/src/main/java/com/ajsnarr/peoplenotes/search/SearchActivity.kt
+++ app/src/main/java/com/ajsnarr/peoplenotes/search/SearchActivity.kt
@@ -2,16 +2,18 @@ package com.ajsnarr.peoplenotes.search
 
 import android.content.Context
 import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
+import android.content.res.Configuration
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ajsnarr.peoplenotes.BaseActivity
 import com.ajsnarr.peoplenotes.R
@@ -22,12 +24,8 @@ import com.ajsnarr.peoplenotes.notes.EditNoteActivity
 import com.ajsnarr.peoplenotes.notes.ViewNoteActivity
 import com.ajsnarr.peoplenotes.util.hideKeyboardFrom
 import com.ajsnarr.peoplenotes.util.max
-import com.google.android.material.textfield.TextInputEditText
-import kotlinx.android.synthetic.main.activity_search.*
 import me.xdrop.fuzzywuzzy.FuzzySearch
 import timber.log.Timber
-import java.lang.IllegalArgumentException
-
 
 /**
  * Min number of characters used in a search
@@ -60,13 +58,12 @@ class SearchActivity : BaseActivity() {
 
     private lateinit var viewModel: SearchViewModel
 
-    private lateinit var addNoteButton: View
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
-
-        binding = ActivitySearchBinding.bind(root)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
         // TODO: Use the ViewModel
@@ -76,13 +73,27 @@ class SearchActivity : BaseActivity() {
         binding.searchBarView.searchActivity = this
 
         // setup recycler view
-        val recycler = recycler_view
         val recyclerManager = LinearLayoutManager(this)
         recyclerAdapter = ResultAdapter(this, RecyclerActionLister(this))
-        recycler.apply {
+        binding.recyclerView.apply {
             layoutManager = recyclerManager
             adapter = recyclerAdapter
         }
+
+        // set up appbar/nav drawer
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ).apply {
+            syncState()
+        }
+        binding.drawerLayout.addDrawerListener(drawerToggle)
 
         // setup search bar
         binding.searchBar.setOnFocusChangeListener { v, hasFocus ->
@@ -92,7 +103,7 @@ class SearchActivity : BaseActivity() {
                 setSearchBarActive(false)
             }
         }
-        binding.searchBar.addTextChangedListener(object: TextWatcher {
+        binding.searchBar.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // do nothing
             }
@@ -100,6 +111,7 @@ class SearchActivity : BaseActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 // do nothing
             }
+
             override fun afterTextChanged(s: Editable?) {
                 Timber.d("Searching with string '${binding.searchBar.text}'")
                 loadNotes()
@@ -111,8 +123,10 @@ class SearchActivity : BaseActivity() {
         }
 
         // setup filter dropdown
-        binding.searchFiltersDropdown.adapter = ArrayAdapter<SearchType>(this,
-            android.R.layout.simple_spinner_item, SearchType.values())
+        binding.searchFiltersDropdown.adapter = ArrayAdapter<SearchType>(
+            this,
+            android.R.layout.simple_spinner_item, SearchType.values()
+        )
         binding.searchFiltersDropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
 
@@ -136,10 +150,28 @@ class SearchActivity : BaseActivity() {
         })
 
         // setup add note button
-        addNoteButton = add_note_button
-        addNoteButton.setOnClickListener {
+        binding.addNoteButton.setOnClickListener {
             // start the edit note activity without passing in an existing note
             startActivity(EditNoteActivity.getCreateNoteIntent(this))
+        }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        return when {
+            drawerToggle.onOptionsItemSelected(item) -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
@@ -157,13 +189,13 @@ class SearchActivity : BaseActivity() {
         // TODO - improve search results shown (search "my " and "my random note" will show but not "my 1")
 
         val searchStr = binding.searchBar.text.toString()
-        val filtered = notes.filter {note ->
+        val filtered = notes.filter { note ->
             // search must be at least MIN_SEARCH_LENGTH chars (otherwise all are shown)
             (searchStr.length < MIN_SEARCH_LENGTH) or
             when (binding.searchFiltersDropdown.selectedItem) {
                 SearchType.ALL -> fuzzyMatch(searchStr, note.name)
                         || note.tags.any { tag -> fuzzyMatch(searchStr, tag.text) }
-                SearchType.NAME ->  fuzzyMatch(searchStr, note.name)
+                SearchType.NAME -> fuzzyMatch(searchStr, note.name)
                 SearchType.TAG -> note.tags.any { tag -> fuzzyMatch(searchStr, tag.text) }
                 else -> true // display everything
             }
@@ -175,11 +207,11 @@ class SearchActivity : BaseActivity() {
                 SearchType.ALL -> max(
                     FuzzySearch.ratio(searchStr, note.name),
                     note.tags.map
-                        {tag -> FuzzySearch.ratio(searchStr, tag.text) }.max() ?: Int.MIN_VALUE
+                    { tag -> FuzzySearch.ratio(searchStr, tag.text) }.max() ?: Int.MIN_VALUE
                 )
                 SearchType.NAME -> FuzzySearch.ratio(searchStr, note.name)
                 SearchType.TAG -> note.tags.map<Tag, Int>
-                    {tag -> FuzzySearch.ratio(searchStr, tag.text) }.max()
+                { tag -> FuzzySearch.ratio(searchStr, tag.text) }.max()
                 else -> throw IllegalArgumentException("Unrecognized search filter type")
             }
         }
@@ -204,11 +236,15 @@ class SearchActivity : BaseActivity() {
 
     private fun loadDefaultNotes() {
         val notes = mutableListOf<Note>(
-            Note(id="123", name="Devin James", tags=mutableListOf<Tag>(Tag("friend"),
-                Tag("charlotesville"), Tag("JMU"), Tag("1"), Tag("1"),
-                Tag("1"), Tag("1"))),
-            Note(id="1233", name="John Smith"),
-            Note(id="1234", name="Openheimer Shmitt")
+            Note(
+                id = "123", name = "Devin James", tags = mutableListOf<Tag>(
+                    Tag("friend"),
+                    Tag("charlotesville"), Tag("JMU"), Tag("1"), Tag("1"),
+                    Tag("1"), Tag("1")
+                )
+            ),
+            Note(id = "1233", name = "John Smith"),
+            Note(id = "1234", name = "Openheimer Shmitt")
         )
         recyclerAdapter.updateResults(notes)
     }
@@ -220,7 +256,7 @@ class SearchActivity : BaseActivity() {
         this.isSearchBarActive = isActive
 
         if (!isActive) {
-            hideKeyboardFrom(binding.searchBar.context, search_bar)
+            hideKeyboardFrom(binding.searchBar.context, binding.searchBar)
             binding.searchBar.clearFocus() // un-click from search bar
         }
     }
